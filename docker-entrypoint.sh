d35262d8269eec99e163556b131f995cae4f8754
Phillip Schichtel <phillip@schich.tel>
2022-02-13 17:53:32 +0100

    automatically create the users

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,16 @@
 export USER="${USER:-$(whoami)}"
 export GROUP="${GROUP:-"$USER"}"
 
+if ! getent passwd "$USER"
+then
+    useradd --system "$USER"
+fi
+
+if ! getent group "$GROUP"
+then
+    groupadd --system "$GROUP"
+fi
+
 envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
